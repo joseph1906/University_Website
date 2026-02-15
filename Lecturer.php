a32db632f9fe3e84cdaf861208092086b51b476d
joseph1906 <josephsajsawasawa@gmail.com>
2026-02-15 18:22:21 +0300

    The head of the file

---
+++ Lecturer.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lecturer Page</title>
+        <link href=""/>
+    </head>
+    <body>
+        <div class="content">
+            <div class="head">
+                <div class="logo">University Name</div>
+        <div class="nav">
+            <li><a href="Lecturer.php">Dashboard</a></li>
+            <li><a href="">R CourseWork</a></li>
+            <li><a href="">Student Grade</a></li>
+        </div>
+        <div class="icon">
+            <li>
+            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAACU0lEQVR4AcSUTYhOURjHz8iwIEo2hIXEwmdsWCpKEbFAJCEbUrYsLG2V8rEQUj6S8lEsLCyIjRSSsJASCyzY+JppZn6/0znTPXfed965U9O8Pb/zf+5z7jnPe+4555kQxvg37glmssBdcAVewS/4CS/hMuyEGdDW2q1gMSMewXe4CntgGUyD6bAc9sJ1+AEPYQEMsVYJ/Ff+w7W8/Rf8p5vQSdCV0Dfmyv4TWw9vYCMUVk+wgV7/8UT0JsyFfXAfeiCbvjFXMY/gPZgMt2E1DFo1wVSiF8HYeXQHuHxkWPtG71a4Bt3gil0hbghOFh2aQzALnsFhaGJ9vHwA3sEi2A7Rqgl2x0gIx1EHINHO0PaDikTTr8fcrxOxN4Q8V7GC+anzedIsrkw/a9Wvxow/sYFVEK26gj8xEsKUpFnOJierj9nPakz+2YCHBCn34HWMhLA0aRb3o4sHFYmmX4/ZscYG3kK06gpuxUgIx5I2lW4GnATtho1UE3hEvxBcB0ehiTnPJQZ4w7+izoWEYpO9kdYdL9Epek/DbOhkHm0vmCfHsQcZ8BuimTk6qXmMmqQXPQKf4Q7UzVu7maC16BOq7+otLw94HrR6AjvcCy+LL9q/xSBYAN+jnn/P/F186xYSztEsgadQmBMUgfTwEa0WLm/pC2ILwU/5AfWzeJrm4HsfLOO4pbVLUL4VwgUCfhYLoOXaFW4j5j2wpOO2tpEmcNOsqhZAP0/r2VpER5LA776SsVZJpJl1SuDZXsGUJkGaW6cE+5my0Sfh/cI6JSheHs3DAAAAAP//P7tC7AAAAAZJREFUAwAYHGYxVIqCEQAAAABJRU5ErkJggg=="/></li>
+            <li><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAABwklEQVR4AcyUSytFURiGNwZGbuUSpQzMDJQMxAy55PIHFDNTCTFQlBSFZCqK/AK3EvILJEYMDEzEzNiE5+Xb2medffbanTrl9D37/c631rfes/Y6excHBf78G4MRNnoLH4byIXJvpNnBPKscQxtUGMpPyWcgMXwG/XSvwRtMQL2hXLUNvvdAzvAZzFrnFHoIWlQoV41SkLgLn0G7VoBzcOPaCq2mseIz+LSuUtM4KYkrhjWfwb1N7DaNSnjv76JFN/cZbFrDDjoGtaCDHkdVQwIdtDQWn8EFXTrEGvQI3uEVDqAapiE8C9Ls8BmoY4tLC+zCCzyDctW2yRMjjYEWeOIyCU3QDMpVI02OJINKWldAt0m35os8imoaW6ZeBrGRy2CQ2Y+wCH2gw0UyQjWNLVHV3F40K+IM9BLTg1XH7BPQw1aFFjmoprEz6g1wCQOQEa6BmvZsxjo6Cnpz6i1KmhGqaWyYavh33icvh79wDfQS0y+/YcYCpI05Jl5B+IyQ/oZr0PlbDlZN04oOP9xFV7TJNeiwQW3d0tTyYDMTDRptku6vpalFT7gmh2soD9wdhP+Un8E8Lln9rkEeaya3FNzgGwAA///0Oi4FAAAABklEQVQDAPDsRjFnAby0AAAAAElFTkSuQmCC"/></li>
+        </div>
+            </div>
+        </div>
+    </body>
+</html>
