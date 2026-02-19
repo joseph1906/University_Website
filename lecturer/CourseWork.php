23fca96ea81584dbd3e45bbcc0ea5df61793cfda
joseph1906 <josephsajsawasawa@gmail.com>
2026-02-19 22:52:51 +0300

    The the head of the different pages

---
+++ lecturer/CourseWork.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="CourseWork.css"/>
+        <title>Coursework</title>
+    </head>
+    <body>
+        <div class="head">
+                <div class="logo"><h2>University Name</h2></div>
+                <ul>
+                    <li><a href="Lecturer.php">Dashboard</a></li>
+                    <li><a href="CourseWork.php">Coursework</a></li>
+                    <li><a href="StudentGrade.php">Student_Grade</a></li>
+                </ul>
+                <div class="icon">
+                    <li><a href=""></a><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAABeklEQVR4AcSUTStFQRjHz5Vi7SUpFhZiYSOsZWMnVvIBJG9loXwIKyUvH0BZiixslKyVZGdhQyIWFnYW/P5j5jZnOnXnnObm9v/NM+eZ55n/mdM9pyVr8u9fDNo51AY8wk8kql2ntg1yCk/Qz+ot7MAAxEq1uxTfQC/U5Ru0kj2FYXiBGeiGWgNUM0vNG4zAMdTlGyyRHQUVjhHP4QMaSTVnFI3DJ0zCAhj5Bssmk2WbxFcoq2catkBa1CB8g0El4BKq6sI26kmYqW+gf4+SVe5efeJJA3SAkW9gEqmHGIM9TPU+KDI10jzMmYVwiDFYtU0u6tLNXVSukBiDfdvpoi7d3EXlCokxWKNTL5siUyPNw5xZCIcYg7Cn1HVqgz7r/mVjltpg2m58b2NSA939tt34wMYkBl1sNgf6VHcSr+EIjIoekV6gMryz0wn0wB3Mg/oJWe4EVyZTbXigbQUmQJ97wp/8E0yRqlVkiL5D+IacfIPcQqqLphv8AgAA//8Lr4r3AAAABklEQVQDADZeRzHr5hZRAAAAAElFTkSuQmCC"/></a></li>
+                    <li><a><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAABwklEQVR4AcyUSytFURiGNwZGbuUSpQzMDJQMxAy55PIHFDNTCTFQlBSFZCqK/AK3EvILJEYMDEzEzNiE5+Xb2medffbanTrl9D37/c631rfes/Y6excHBf78G4MRNnoLH4byIXJvpNnBPKscQxtUGMpPyWcgMXwG/XSvwRtMQL2hXLUNvvdAzvAZzFrnFHoIWlQoV41SkLgLn0G7VoBzcOPaCq2mseIz+LSuUtM4KYkrhjWfwb1N7DaNSnjv76JFN/cZbFrDDjoGtaCDHkdVQwIdtDQWn8EFXTrEGvQI3uEVDqAapiE8C9Ls8BmoY4tLC+zCCzyDctW2yRMjjYEWeOIyCU3QDMpVI02OJINKWldAt0m35os8imoaW6ZeBrGRy2CQ2Y+wCH2gw0UyQjWNLVHV3F40K+IM9BLTg1XH7BPQw1aFFjmoprEz6g1wCQOQEa6BmvZsxjo6Cnpz6i1KmhGqaWyYavh33icvh79wDfQS0y+/YcYCpI05Jl5B+IyQ/oZr0PlbDlZN04oOP9xFV7TJNeiwQW3d0tTyYDMTDRptku6vpalFT7gmh2soD9wdhP+Un8E8Lln9rkEeaya3FNzgGwAA///0Oi4FAAAABklEQVQDAPDsRjFnAby0AAAAAElFTkSuQmCC"/></a></li>
+                </div>
+            </div>
+    </body>
+</html>
